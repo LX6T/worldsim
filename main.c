ff2ac95cf95fb2d453d909af4f31c1f61e7391b2
LX6T <alex@pullen.net.nz>
2025-02-20 16:23:53 +1300

    Implemented random walk

--- main.c
+++ main.c
@@ -1,41 +1,73 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define WINDOW_TITLE "04 Colors"
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-#define IMAGE_FLAGS IMG_INIT_PNG
+#define WINDOW_TITLE "Random Walk"
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 1000
 
 struct Game {
     SDL_Window *window;
     SDL_Renderer *renderer;
-    SDL_Texture *background;
 };
 
 void game_cleanup(struct Game *game, int exit_status);
-bool load_media(struct Game *game);
 bool sdl_initialize(struct Game *game);
 
 int main() {
     struct Game game = {
         .window = NULL,
         .renderer = NULL,
-        .background = NULL,
     };
 
     if (sdl_initialize(&game)) {
         game_cleanup(&game, EXIT_FAILURE);
     }
 
-    if (load_media(&game)) {
-        game_cleanup(&game, EXIT_FAILURE);
-    }
+    srand(time(NULL));
+
+    int x = 25;
+    int y = 25;
 
     while (true) {
+
+        uint8_t dir = rand() % 4;
+
+        switch (dir)
+        {
+            case 0:
+                x -= 1;
+                y -= 1;
+                break;
+            case 1:
+                x += 1;
+                y -= 1;
+                break;
+            case 2:
+                x += 1;
+                y += 1;
+                break;
+            case 3:
+                x -= 1;
+                y += 1;
+                break;
+        }
+
+        if (x > 50 || x < 0 || y > 50 || y < 0)
+        {
+            x = (x + 50) % 50;
+            y = (y + 50) % 50;
+        }
+        
+        SDL_SetRenderDrawColor(game.renderer,
+            (dir == 0 || dir == 1) ? 255 : 0,
+            (dir == 1 || dir == 2) ? 255 : 0,
+            (dir == 3) ? 255 : 0,
+            255);
+        SDL_RenderDrawPoint(game.renderer, x, y);
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -47,10 +79,6 @@ int main() {
                 case SDL_SCANCODE_ESCAPE:
                     game_cleanup(&game, EXIT_SUCCESS);
                     break;
-                case SDL_SCANCODE_SPACE:
-                    SDL_SetRenderDrawColor(game.renderer, rand() % 256,
-                                           rand() % 256, rand() % 256, 255);
-                    break;
                 default:
                     break;
                 }
@@ -58,13 +86,10 @@ int main() {
                 break;
             }
         }
-        SDL_RenderClear(game.renderer);
-
-        SDL_RenderCopy(game.renderer, game.background, NULL, NULL);
 
         SDL_RenderPresent(game.renderer);
 
-        SDL_Delay(16);
+        SDL_Delay(4);
     }
 
     game_cleanup(&game, EXIT_SUCCESS);
@@ -73,7 +98,6 @@ int main() {
 }
 
 void game_cleanup(struct Game *game, int exit_status) {
-    SDL_DestroyTexture(game->background);
     SDL_DestroyRenderer(game->renderer);
     SDL_DestroyWindow(game->window);
     IMG_Quit();
@@ -84,12 +108,6 @@ void game_cleanup(struct Game *game, int exit_status) {
 bool sdl_initialize(struct Game *game) {
     if (SDL_Init(SDL_INIT_EVERYTHING)) {
         fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
-        return true;
-    }
-
-    int img_init = IMG_Init(IMAGE_FLAGS);
-    if ((img_init & IMAGE_FLAGS) != IMAGE_FLAGS) {
-        fprintf(stderr, "Error initializing SDL_image: %s\n", IMG_GetError());
         return true;
     }
 
@@ -106,18 +124,9 @@ bool sdl_initialize(struct Game *game) {
         fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
         return true;
     }
+    SDL_RenderSetScale(game->renderer, 20, 20);
 
     srand((unsigned)time(NULL));
-
-    return false;
-}
-
-bool load_media(struct Game *game) {
-    game->background = IMG_LoadTexture(game->renderer, "images/background.png");
-    if (!game->background) {
-        fprintf(stderr, "Error creating Texture: %s\n", IMG_GetError());
-        return true;
-    }
 
     return false;
 }
